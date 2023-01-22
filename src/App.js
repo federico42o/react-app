2f368fb9df2cdbdbc8612aebef31b1f6ab192337
federicos1 <fedes7777@gmail>
2023-01-22 08:53:10 -0300

    add: twitch api

--- src/App.js
+++ src/App.js
@@ -5,26 +5,58 @@ import {useState,useEffect} from 'react'
 
 function App() {
 
-  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
 
-  useEffect(() => {
-    api.get('api/v1/organization/')
+useEffect(() => {
+  try {
+
+    api.get('/streams')
       .then(response => {
+        
         setData(response.data);
+        console.log(response.data)
+        setIsLoading(false);
       });
-  }, []);
+  } catch (error) {
+    console.error(error);
 
-  return (
-    <div>
-      {data.map(item => (
-        <div key={item.id}>
-          <h2>{item.name}</h2>
-          <p>{item.address}</p>
-          <p>{item.cuit}</p>
+
+  }
+  },   []);
+
+if (isLoading) {
+  return <p>Loading...</p>;
+}
+
+
+
+return (
+  <div style={{textAlign:"center",backgroundColor: "#fff",color: "#9d4de3"}}>
+
+  <h1>20 Most Viewed Streamers on Twitch </h1>
+
+  <div style={{}}>
+    {data.data.map((item) => (
+      <a href={'https://twitch.tv/' + item.user_name} style={{textDecoration: 'none'}} target="_blank">
+      <div key={item.id} style={{border:'2px solid black'}} >
+        <div className='cuadro' style={{display:'flex',gap:"30px",alignItems:"center",width:'100%', color:"black",justifyContent:"center"}}>
+        <p>{item.user_name}</p>
+        <p>playing:</p>
+        <p>{item.game_name}</p>
+        <p>for</p>
+        <p>{item.viewer_count}</p>
+        <p>viewers</p>
+
         </div>
-      ))}
-    </div>
-  );
+      </div>
+        </a>
+    ))}
+  </div>
+</div>
+);
+
+
 }
 export default App;
 
